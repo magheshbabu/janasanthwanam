9c387eeb9f7887a672a82223837a20ab22ff4a64
Shyam S <shyam@shyams-imac.attlocal.net>
2017-08-19 13:37:29 -0500

    file moved to disk

---
+++ src/main/java/com/gsr/janasanth/config/ApekshaConfig.java
@@ -0,0 +1,13 @@
+package com.gsr.janasanth.config;
+
+public class ApekshaConfig {
+	String fileRoot;
+
+	public String getFileRoot() {
+		return fileRoot;
+	}
+
+	public void setFileRoot(String fileRoot) {
+		this.fileRoot = fileRoot;
+	}
+}
--- src/main/java/com/gsr/janasanth/service/impl/ApekshaServiceImpl.java
+++ src/main/java/com/gsr/janasanth/service/impl/ApekshaServiceImpl.java
@@ -1,6 +1,8 @@
 package com.gsr.janasanth.service.impl;
 
 import com.gsr.janasanth.service.ApekshaService;
+import com.gsr.janasanth.service.FileService;
+import com.gsr.janasanth.config.ApplicationProperties;
 import com.gsr.janasanth.domain.Apeksha;
 import com.gsr.janasanth.repository.ApekshaRepository;
 import com.gsr.janasanth.repository.search.ApekshaSearchRepository;
@@ -16,6 +18,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 /**
  * Service Implementation for managing Apeksha.
  */
@@ -30,11 +36,17 @@ public class ApekshaServiceImpl implements ApekshaService{
     private final ApekshaMapper apekshaMapper;
 
     private final ApekshaSearchRepository apekshaSearchRepository;
+    
+    private final FileService fileService;
+    
+    private final ApplicationProperties properties;
 
-    public ApekshaServiceImpl(ApekshaRepository apekshaRepository, ApekshaMapper apekshaMapper, ApekshaSearchRepository apekshaSearchRepository) {
+    public ApekshaServiceImpl(ApekshaRepository apekshaRepository, ApekshaMapper apekshaMapper, ApekshaSearchRepository apekshaSearchRepository, FileService fileService, ApplicationProperties properties) {
         this.apekshaRepository = apekshaRepository;
         this.apekshaMapper = apekshaMapper;
         this.apekshaSearchRepository = apekshaSearchRepository;
+        this.fileService = fileService;
+        this.properties = properties;
     }
 
     /**
@@ -47,13 +59,30 @@ public class ApekshaServiceImpl implements ApekshaService{
     public ApekshaDTO save(ApekshaDTO apekshaDTO) {
         log.debug("Request to save Apeksha : {}", apekshaDTO);
         Apeksha apeksha = apekshaMapper.toEntity(apekshaDTO);
+        byte[] fileContent = apeksha.getAdditionalDocuments();
+        apeksha.setAdditionalDocuments(null);
+        apeksha = apekshaRepository.save(apeksha);
+        String fileName = UUID.randomUUID().toString();
+        try {
+			fileService.saveBytestoDisk(fileContent, properties.getApeksha().getFileRoot() + apeksha.getId() + "/", fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        createApplicationNumber(apeksha);
+        apeksha.additionalDocuments(fileName.getBytes(Charset.forName("UTF-8")));
         apeksha = apekshaRepository.save(apeksha);
         ApekshaDTO result = apekshaMapper.toDto(apeksha);
         apekshaSearchRepository.save(apeksha);
         return result;
     }
 
-    /**
+    private void createApplicationNumber(Apeksha apeksha) {
+    	String applicationNumber = apeksha.getId() + "/2017/TVM";
+    	apeksha.setApplicationNumber(applicationNumber);
+	}
+
+	/**
      *  Get all the apekshas.
      *
      *  @param pageable the pagination information
@@ -64,10 +93,26 @@ public class ApekshaServiceImpl implements ApekshaService{
     public Page<ApekshaDTO> findAll(Pageable pageable) {
         log.debug("Request to get all Apekshas");
         return apekshaRepository.findAll(pageable)
-            .map(apekshaMapper::toDto);
+        		.map(a -> toDto(a));
+            //.map(apekshaMapper::toDto);
     }
 
-    /**
+    private ApekshaDTO toDto(Apeksha apeksha) {
+    	
+    	ApekshaDTO dto = apekshaMapper.toDto(apeksha);
+    	if(null != dto.getAdditionalDocuments() && dto.getAdditionalDocuments().length > 0){
+    		String fileName = new String(dto.getAdditionalDocuments(), Charset.forName("UTF-8"));
+    		try {
+				dto.setAdditionalDocuments(fileService.readBytesFromDisk(properties.getApeksha().getFileRoot() + apeksha.getId() + "/", fileName));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+		return dto;
+	}
+
+	/**
      *  Get one apeksha by id.
      *
      *  @param id the id of the entity
@@ -78,7 +123,7 @@ public class ApekshaServiceImpl implements ApekshaService{
     public ApekshaDTO findOne(Long id) {
         log.debug("Request to get Apeksha : {}", id);
         Apeksha apeksha = apekshaRepository.findOne(id);
-        return apekshaMapper.toDto(apeksha);
+        return toDto(apeksha);
     }
 
     /**
@@ -105,6 +150,6 @@ public class ApekshaServiceImpl implements ApekshaService{
     public Page<ApekshaDTO> search(String query, Pageable pageable) {
         log.debug("Request to search for a page of Apekshas for query {}", query);
         Page<Apeksha> result = apekshaSearchRepository.search(queryStringQuery(query), pageable);
-        return result.map(apekshaMapper::toDto);
+        return result.map(a -> toDto(a));
     }
 }
